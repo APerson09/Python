04f036f788d8c7cc2389ccb00c477ea880f33d65
APerson09 <ethanhuang63@gmail.com>
2026-09-16 13:28:50 -0700

    Created ToDoList

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+reminders = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-  return "Hello, Flask is working!"
+  if request.method == "POST":
+    new_reminder = request.form.get("reminder_txt")
 
+    if new_reminder:
+      reminders.append(new_reminder)
+      return redirect("/")
+  
+  return render_template("index.html", reminders=reminders)
+
+@app.route("/delete/<int:index>")
+def deleteIndex(index):
+  if 0 <= index < len(reminders):
+        reminders.pop(index)
+  return redirect("/")
 
 if __name__ == "__main__":
   app.run(debug=True)
